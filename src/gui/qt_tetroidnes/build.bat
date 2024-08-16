8ed12e30d61e17ccc87b5f3823e2c098a8f3f7b6
Theo <95878514+TheoW03@users.noreply.github.com>
2024-08-16 00:48:52 -0400

    QT works I Added a bat script for building the GUI :3

---
+++ src/gui/qt_tetroidnes/build.bat
@@ -0,0 +1,5 @@
+@echo off
+cmake -G "MinGW Makefiles" -DCMAKE_PREFIX_PATH=C:\Qt\6.7.2\mingw_64\  -DCMAKE_CXX_FLAGS="-std=c++17"  -S . -B build
+cmake --build build --config Release
+cd build
+qt_tetroidnes.exe
