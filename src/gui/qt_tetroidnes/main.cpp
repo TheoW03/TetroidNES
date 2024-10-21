030e1c9460e3ea81f8a279c28d60ad6d9f4f38bb
TheoW03 <21theowillis@gmail.com>
2024-10-21 12:17:54 -0400

    clean up.

--- src/gui/qt_tetroidnes/main.cpp
+++ src/gui/qt_tetroidnes/main.cpp
@@ -18,46 +18,6 @@
 #include <QDir>
 #include <Qt/log.h>
 
-// QtMessageHandler originalHandler = nullptr;
-
-// void logToFile(QtMsgType type, const QMessageLogContext &context, const QString &msg)
-// {
-//     QString message = qFormatLogMessage(type, context, msg);
-//     std::filesystem::create_directories("logs");
-//     std::time_t t = std::time(0); // t is an integer type
-//     // char *intStr = itoa(t);
-//     // std::string str = std::string(intStr);
-//     std::string time_stamp = std::to_string(t);
-//     auto file = "logs/log_" + time_stamp + ".txt";
-//     static FILE *f = fopen(file.c_str(), "a");
-//     fprintf(f, "%s\n", qPrintable(message));
-//     fflush(f);
-//     if (originalHandler)
-//     {
-//         originalHandler(type, context, msg);
-//     }
-// }
-
-// void check_log_dir()
-// {
-//     const short int max_size = 32;
-//     QStringList log_dir = QDir("logs", "*.txt", QDir::Name, QDir::Files).entryList();
-//     auto log_dir_size = log_dir.length();
-
-//     if (log_dir_size > max_size)
-//     {
-//         auto logs_to_be_deleted = log_dir.sliced(0, log_dir_size - max_size);
-//         qDebug() << "Over" << max_size << "logs reached, deleting older logs:" << logs_to_be_deleted;
-//         for (auto &file_name : logs_to_be_deleted)
-//         {
-//             if (!QFile::remove("logs/" + file_name))
-//             {
-//                 qWarning() << "Could not remove log file:" << file_name;
-//             }
-//         }
-//     }
-// }
-
 int main(int argc, char *argv[])
 {
     // originalHandler = qInstallMessageHandler(logToFile);
@@ -85,10 +45,6 @@ int main(int argc, char *argv[])
     }
 
     MainWindow w;
-
-    // int exit_code = a.exec();
-
-    // qInfo() << "ENDING" << a.applicationName() << "VERSION" << a.applicationVersion() << "EXIT CODE" << exit_code;
     w.show();
 
     return a.exec();
