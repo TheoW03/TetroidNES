aa867fda8b8da51c6b07194f250c9b5a9f0e9ae3
TheoW03 <21theowillis@gmail.com>
2024-12-16 14:05:13 -0500

    Switched over to Pascal_Snake_Case for source file name

---
+++ src/gui/qt_tetroidnes/Main.cpp
@@ -0,0 +1,38 @@
+#define VERSION "1.0.0" // DO NOT EDIT: CMake handles this
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+#include <QtLogging>
+
+#include <Qt/mainwindow.h>
+#include <Qt/log.h>
+
+int main(int argc, char *argv[])
+{
+    // originalHandler = qInstallMessageHandler(logToFile);
+    InitLogs();
+    // int &c = argc
+    QApplication a(argc, argv);
+    a.setApplicationName("TetroidNES");
+
+    a.setApplicationVersion(VERSION);
+
+    qInfo() << "STARTING" << a.applicationName() << "VERSION" << a.applicationVersion();
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages)
+    {
+        const QString baseName = "tetroidnes_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName))
+        {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
