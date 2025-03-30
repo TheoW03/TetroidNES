18410753707e04771406d16225712167dff7652b
TheoW03 <21theowillis@gmail.com>
2025-03-30 14:13:02 -0400

    added impl
    for ppu addre class

--- src/emulator/ppu_address.cpp
+++ src/emulator/ppu_address.cpp
@@ -4,16 +4,32 @@ PPUAddress::PPUAddress()
 {
 }
 
+void PPUAddress::write_8bit(uint8_t value)
+{
+    if (this->high_ptr)
+    {
+        this->ppuAddr.hi = value;
+    }
+    else
+    {
+        this->ppuAddr.lo = value;
+    }
+    this->high_ptr = !high_ptr;
+}
+
 void PPUAddress::write_16bit(uint16_t value)
 {
+    this->ppuAddr.val = value;
 }
 uint16_t PPUAddress::read_16bit()
 {
-    return 0;
+    return this->ppuAddr.val;
 }
 
 void PPUAddress::reset()
 {
+    this->ppuAddr.val = 0;
+    this->high_ptr = true;
 }
 
 void PPUAddress::log()
--- src/include/Emulator/component_registers.h
+++ src/include/Emulator/component_registers.h
@@ -22,8 +22,20 @@ public:
 class PPUAddress : public ComponentRegister
 {
 private:
+    bool high_ptr;
+    union
+    {
+        struct
+        {
+            unsigned lo : 8;
+            unsigned hi : 8;
+        };
+        uint16_t val;
+    } ppuAddr;
+
 public:
     PPUAddress();
+    void write_8bit(uint8_t value) override;
     void write_16bit(uint16_t value) override;
     uint16_t read_16bit() override;
     void reset() override;
