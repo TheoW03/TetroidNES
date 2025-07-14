a713382a058b7c76f443d1f489b56fe2e0bf7d17
Wolfmyths <tdellegrin@gmail.com>
2025-07-14 02:35:41 -0400

    build.bat troubleshooting
    [Refactor] Added error message if environmental path is not set to find windeployqt6.exe

--- build.bat
+++ build.bat
@@ -1,6 +1,20 @@
 @echo off
+
+set QT_WIN_DEPLOY_PATH="%QT_PATH%\bin\windeployqt6.exe"
+
 cmake -G "MinGW Makefiles" -S . -B build
 cmake --build build --config Release
+
 cd build
-%QT_PATH%\bin\windeployqt6.exe TetroidNES.exe
+
+IF EXIST "%QT_WIN_DEPLOY_PATH%" (
+    %QT_WIN_DEPLOY_PATH% TetroidNES.exe
+) ELSE (
+    echo WARNING: CANNOT FIND 'windeployqt6.exe'
+    echo IN: %QT_WIN_DEPLOY_PATH%
+    echo Without this path, TetroidNES may build correctly but without the required DLL files crashing on startup
+    echo Set env variable QT_PATH to the correct path
+    echo Example: 'path-to-qt-root\\Qt\\6.9.1\\mingw_64'
+)
+
 TetroidNES.exe
